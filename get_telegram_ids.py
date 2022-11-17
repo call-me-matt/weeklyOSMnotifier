de9bb7218e13ef5603c9abe75eb6e980b9e0f393
Matthias <matthias@peanut>
2022-11-17 19:39:38 +0100

    add telegram id-scanner
    Signed-off-by: Matthias <matthias@peanut>

---
+++ get_telegram_ids.py
@@ -0,0 +1,28 @@
+# this is a helper script to find out chat ids of telegram groups that
+# have added our bot "theweeklyOSMbot" to their groups.
+
+# How to use:
+# source spamenv/bin/activate
+# python get_telegram_ids.py
+# deactivate
+
+import telepot
+import yaml
+
+with open('configs/secrets/weeklysecrets.yaml') as f:
+    # use safe_load instead load
+    secrets = yaml.safe_load(f)
+
+bot = telepot.Bot(secrets["telegram_TOKEN"])
+resp = bot.getUpdates()
+
+chats = []
+
+for update in resp:
+    try:
+        info = update['message']['chat']
+        if not info in chats:
+            chats.append(info)
+            print(info)
+    except KeyError:
+        continue
