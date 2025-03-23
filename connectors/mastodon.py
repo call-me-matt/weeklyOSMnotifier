8983ee746a64de419ef73882bf0224e3a949f860
Matthias <matthias@matthiasheinisch.de>
2025-03-23 22:28:55 +0100

    refactor: break down mastodon function into modular components.
    Signed-off-by: Matthias <matthias@matthiasheinisch.de>

--- connectors/mastodon.py
+++ connectors/mastodon.py
@@ -2,43 +2,62 @@ from mastodon import Mastodon
 from PIL import Image
 
 
+def login(self):
+    return Mastodon(
+        access_token=self.mastodon_TOKEN, api_base_url=self.mastodon_INSTANCE
+    )
+
+
+def upload_pic(self, mastodon):
+    try:
+        self.logger.debug("sending toot with image")
+        pic = mastodon.media_post(self.pic)
+        return [pic.id]
+    except Exception as e:
+        self.logger.error(f"failed to upload picture to Mastodon: {e}")
+        # check if it is an animated gif (not supported by mastodon)
+        try:
+            image = Image.open(self.pic)
+            if image.is_animated:
+                self.logger.info("animated gif not supported, extracting frame")
+                image.seek(0)
+                image.save(self.pic + ".png", type="png")
+                pic = mastodon.media_post(self.pic + ".png")
+                return [pic.id]
+        except:
+            self.logger.error(
+                f"failed to extract frame from animated gif. continuing without image. {e}"
+            )
+        return None
+
+
+def pin_post(self, mastodon):
+    try:
+        if self.do_unpin_mastodon:
+            for pinned_toot in mastodon.account_statuses(mastodon.me().id, pinned=True):
+                self.logger.info(f"...unpinning previous toot {pinned_toot.id}")
+                resp = mastodon.status_unpin(pinned_toot.id)
+                self.logger.debug(f"{resp}")
+        resp = mastodon.status_pin(toot.id)
+        self.logger.info(f"...pinned toot")
+        self.logger.debug(f"{resp}")
+    except Exception as e:
+        self.logger.error(f"failed to pin mastodon status: {e}")
+
+
 def post(self):
     try:
-        mastodon = Mastodon(
-            access_token=self.mastodon_TOKEN, api_base_url=self.mastodon_INSTANCE
-        )
+        mastodon = self.login()
     except Exception as e:
         self.logger.error(f"could not connect to mastodon - {e}")
         return
 
     for recipient in self.mastodon_to:
-
         self.logger.info(f"...toot to {recipient}")
-
         media = None
         # upload picture if applicable
         if self.pic:
-            try:
-                self.logger.debug("sending toot with image")
-                pic = mastodon.media_post(self.pic)
-                media = [pic.id]
-            except Exception as e:
-                self.logger.error(f"failed to upload picture to Mastodon: {e}")
-                media = None
-                # check if it is an animated gif (not supported by mastodon)
-                try:
-                    image = Image.open(self.pic)
-                    if image.is_animated:
-                        self.logger.info("animated gif not supported, extracting frame")
-                        image.seek(0)
-                        image.save(self.pic + ".png", type="png")
-                        pic = mastodon.media_post(self.pic + ".png")
-                        media = [pic.id]
-                except:
-                    self.logger.error(
-                        f"failed to extract frame from animated gif. continuing without image. {e}"
-                    )
-                    media = None
+            media = self.upload_pic(mastodon)
 
         # toot!
         if self.lang == "int":
@@ -57,21 +76,7 @@ def post(self):
             self.logger.debug(f"{toot}")
             # pin status
             if self.do_pin_mastodon:
-                try:
-                    if self.do_unpin_mastodon:
-                        for pinned_toot in mastodon.account_statuses(
-                            mastodon.me().id, pinned=True
-                        ):
-                            self.logger.info(
-                                f"...unpinning previous toot {pinned_toot.id}"
-                            )
-                            resp = mastodon.status_unpin(pinned_toot.id)
-                            self.logger.debug(f"{resp}")
-                    resp = mastodon.status_pin(toot.id)
-                    self.logger.info(f"...pinned toot")
-                    self.logger.debug(f"{resp}")
-                except Exception as e:
-                    self.logger.error(f"failed to pin mastodon status: {e}")
+                self.pin_post(mastodon)
         else:
             # send direct messages
             try:
